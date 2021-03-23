a5f5b084883ea77c651a53b85e2580a6c7051803
Velibor Mandzo <>
2021-03-23 16:29:17 +0100

    update

--- src/test/java/helpers/BaseHelper.java
+++ src/test/java/helpers/BaseHelper.java
@@ -1,6 +1,9 @@
 package helpers;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -8,5 +11,15 @@ public class BaseHelper
 {
     protected static WebDriver driver = new ChromeDriver();
     protected static WebDriverWait wdWait = new WebDriverWait(driver, 20);
+    protected static JavascriptExecutor js = (JavascriptExecutor) driver;
+
+    public static WebElement returnElementAttValue (String attributeName, String attributeValue)
+    {
+        String selector = "[" + attributeName + "=" + attributeValue + "]";
+    WebElement element = driver.findElement(By.cssSelector(selector));
+    return element;
+    }
+
+
 
 }
