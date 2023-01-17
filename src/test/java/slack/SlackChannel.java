95ae10cb9ef329e519f1eefbf00f8b36168b4c0a
velibor-mandzo <44603149+velibor-mandzo@users.noreply.github.com>
2023-01-17 17:31:42 +0100

    Update SlackChannel.java

--- src/test/java/slack/SlackChannel.java
+++ src/test/java/slack/SlackChannel.java
@@ -5,10 +5,9 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum SlackChannel {
-
-    QA_DEMO("tec_pmd_qa_demo", "https://hooks.slack.com/services/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"),
-    QARevealed("qacourse","https://hooks.slack.com/services/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")
+public enum SlackChannel 
+{
+QARevealed("qacourse","https://hooks.slack.com/services/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")
     ;
 
     private final String channelName;
